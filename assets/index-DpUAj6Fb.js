b58171271f20c7ef8d4b5d3672651a6c1cba62d0
GroAnita <gro.anita.brathen@gmail.com>
2026-01-23 09:10:44 +0100

    Updates

--- assets/index-DpUAj6Fb.js
+++ assets/index-DpUAj6Fb.js
@@ -1,21 +1,21 @@
-(function(){const a=document.createElement("link").relList;if(a&&a.supports&&a.supports("modulepreload"))return;for(const t of document.querySelectorAll('link[rel="modulepreload"]'))i(t);new MutationObserver(t=>{for(const s of t)if(s.type==="childList")for(const r of s.addedNodes)r.tagName==="LINK"&&r.rel==="modulepreload"&&i(r)}).observe(document,{childList:!0,subtree:!0});function n(t){const s={};return t.integrity&&(s.integrity=t.integrity),t.referrerPolicy&&(s.referrerPolicy=t.referrerPolicy),t.crossOrigin==="use-credentials"?s.credentials="include":t.crossOrigin==="anonymous"?s.credentials="omit":s.credentials="same-origin",s}function i(t){if(t.ep)return;t.ep=!0;const s=n(t);fetch(t.href,s)}})();function l(){return`
+(function(){const s=document.createElement("link").relList;if(s&&s.supports&&s.supports("modulepreload"))return;for(const t of document.querySelectorAll('link[rel="modulepreload"]'))i(t);new MutationObserver(t=>{for(const a of t)if(a.type==="childList")for(const r of a.addedNodes)r.tagName==="LINK"&&r.rel==="modulepreload"&&i(r)}).observe(document,{childList:!0,subtree:!0});function c(t){const a={};return t.integrity&&(a.integrity=t.integrity),t.referrerPolicy&&(a.referrerPolicy=t.referrerPolicy),t.crossOrigin==="use-credentials"?a.credentials="include":t.crossOrigin==="anonymous"?a.credentials="omit":a.credentials="same-origin",a}function i(t){if(t.ep)return;t.ep=!0;const a=c(t);fetch(t.href,a)}})();const l="/JS2_CA/assets/protectionspell-B3fmzc5v.jpg";function n(){return`
     <section class="p-6 space-y-6">
 
       <h1 class="font-[Cinzel] text-purple-400 font-bold text-xl">
         Spellcasting Feed
       </h1>
 
-      <article class="relative border border-gray-700 rounded-lg p-4 bg-[var(--color-bg-surface)] shadow-sm">
+      <article class="relative border border-gray-700 rounded-lg p-4 bg-[var(--color-bg-surface)] shadow-sm w-2/3 mx-auto">
         <i class="fa-solid fa-pen-to-square absolute top-3 right-3 text-gray-400 hover:text-purple-400 cursor-pointer transition"></i>
         <div class="border-b border-gray-700 pb-4">
           <figure >
            <div class="flex flex-row justify-evenly items-center mb-4 gap-4">
             <img class="h-56 rounded-md mb-2"
-              src="/JS2_CA/images/protectionspell.jpg"
+              src="${l}"
               alt="LunaHex Illustration - Protection Spell"
             />
             <img class="h-56 rounded-md mb-2"
-              src="/JS2_CA/images/protectionspell.jpg"
+              src="${l}"
               alt="LunaHex Illustration - Protection Spell"
             />
             </div>
@@ -54,13 +54,13 @@
       </article>
 
     </section>
-  `}function c(){return`
+  `}function d(){return`
     <h1>Welcome to the About Page</h1>
     <p>This is the about page of our application.</p>
-  `}function d(){return`
+  `}function p(){return`
     <h1>Welcome to the Contact Page</h1>
     <p>This is the contact page of our application.</p>
-  `}function p(){return`
+  `}const f="/JS2_CA/assets/storiesplaceholder-BEkCxyvR.png";function u(){return`
     <section
       class="
         flex items-center gap-4
@@ -74,7 +74,7 @@
         <div class="flex flex-col items-center gap-1 shrink-0">
           <div
             class="
-              w-14 h-14
+              w-28 h-28 
               rounded-full
               border-2 border-gray-300/70
               p-0.5
@@ -83,28 +83,24 @@
             "
           >
             <img
-              src="/JS2_CA/images/megibunadlite.jpg"
+              src="${f}"
               alt="Active user"
               class="w-full h-full rounded-full object-cover"
             />
           </div>
-
-          <span class="text-xs text-gray-400">
-            Coven
-          </span>
         </div>
       `).join("")}
     </section>
-  `}function f(){return`
+  `}function h(){return`
     <section class="footer-section">
     <p>&copy; 2024 Hex & Chill. All rights reserved.</p>
     </section>
-  `}function u(){const e=`
+  `}const m="/JS2_CA/assets/witchicon-BgI6EGWp.png";function g(){const e=`
     flex flex-col items-center gap-2
     text-gray-300 opacity-80
     hover:text-purple-400 hover:opacity-100
     transition
-  `,a=`
+  `,s=`
     w-12 h-12
     rounded-full
     border-2 border-gray-300/60
@@ -114,40 +110,40 @@
   `;return`
     <aside class="hidden md:flex w-64 flex-col bg-[var(--color-bg-surface)] px-6 py-8 border-r border-gray-700">
     <h1 class="font-[Cinzel] text-2xl font-bold text-purple-400 mb-6 mx-auto">Hex & Chill</h1>
-    <img src="/JS2_CA/images/icon/witchicon.png" alt="Hex & Chill Logo" class="w-20 h-20 mx-auto mb-4"/>
+    <img src="${m}" alt="Hex & Chill Logo" class="w-20 h-20 mx-auto mb-4"/>
       <h2 class="font-[Cinzel] mb-10 text-xl mx-auto font-semibold text-purple-400">🕯️ Coven</h2>
 
       <nav class="flex flex-col gap-8">
 
       <a href="/" data-link class="${e}">
-          <div class="${a}">
+          <div class="${s}">
             <i class="fa-solid fa-book"></i>
           </div>
           <span class="text-xs font-[Cinzel]">Grimoire</span>
         </a>
 
         <a href="/" data-link class="${e}">
-          <div class="${a}">
+          <div class="${s}">
             <i class="fa-solid fa-crow"></i>
           </div>
           <span class="text-xs font-[Cinzel]">Familiars</span>
         </a>
 
         <a href="/" data-link class="${e}">
-          <div class="${a}">
+          <div class="${s}">
             <i class="fa-solid fa-broom"></i>          </div>
           <span class="text-xs font-[Cinzel]">Scry</span>
         </a>
 
         <a href="/" data-link class="${e}">
-          <div class="${a}">
+          <div class="${s}">
             <i class="fa-solid fa-hat-wizard"></i>
           </div>
           <span class="text-xs font-[Cinzel]">Coven Profile</span>
         </a>
 
         <a href="/search" data-link class="${e}">
-          <div class="${a}">
+          <div class="${s}">
             <i class="fa-solid fa-moon"></i>
           </div>
           <span class="text-xs font-[Cinzel]">Cast a Spell</span>
@@ -155,7 +151,7 @@
 
 
         <a href="/search" data-link class="${e}">
-          <div class="${a}">
+          <div class="${s}">
             <i class="fa-solid fa-right-to-bracket"></i>
           </div>
           <span class="text-xs font-[Cinzel]">Enter the Circle</span>
@@ -163,4 +159,4 @@
 
       </nav>
     </aside>
-  `}const g={"/":l,"/about":c,"/contact":d};function o(){const e=window.location.pathname,a=g[e]||l;document.getElementById("sidenav").innerHTML=u(),document.getElementById("header").innerHTML=p(),document.getElementById("app").innerHTML=a(),document.getElementById("footer").innerHTML=f()}function m(e){history.pushState(null,null,e),o()}document.addEventListener("click",e=>{e.target.matches("a[data-link]")&&(e.preventDefault(),m(e.target.href))});window.addEventListener("popstate",o);o();
+  `}const x={"/":n,"/about":d,"/contact":p};function o(){const e=window.location.pathname,s=x[e]||n;document.getElementById("sidenav").innerHTML=g(),document.getElementById("header").innerHTML=u(),document.getElementById("app").innerHTML=s(),document.getElementById("footer").innerHTML=h()}function b(e){history.pushState(null,null,e),o()}document.addEventListener("click",e=>{e.target.matches("a[data-link]")&&(e.preventDefault(),b(e.target.href))});window.addEventListener("popstate",o);o();
