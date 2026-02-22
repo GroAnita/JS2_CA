5c4c4149f7410809d25114b104248573ea85d3b1
GroAnita <gro.anita.brathen@gmail.com>
2026-02-22 13:41:20 +0100

    some small code changes and prettying up

--- src/router/routeguards.js
+++ src/router/routeguards.js
@@ -1,39 +1,39 @@
-import { isLoggedIn } from "../state/authstate";
+import { isLoggedIn } from '../state/authstate';
 
 // router/protects routes like /profile from unauthenticated users
 
 //redirects unauthenticated users to home page/ guests
 export function requireAuth() {
-    if (!isLoggedIn()) {
-        //my user is not logged in, redirect to home page
-        history.pushState(null, null, "/");
-        return false;
-    }
-    return true;
+  if (!isLoggedIn()) {
+    //my user is not logged in, redirect to home page
+    history.pushState(null, null, '/');
+    return false;
+  }
+  return true;
 }
 
 //redirects logged in users away from login/register pages
 export function redirectIfLoggedIn() {
-    if (isLoggedIn()) {
-        //user is logged in, redirect to home page
-        history.pushState(null, null, "/");
-        return false;
-    }
-    return true;
+  if (isLoggedIn()) {
+    //user is logged in, redirect to home page
+    history.pushState(null, null, '/');
+    return false;
+  }
+  return true;
 }
 
 // protecting a route to only allow logged in users
 export function protectedRoute(view) {
-    return() => {
-        if (!requireAuth()) return "";
-            return view();
-    };
-}        
+  return () => {
+    if (!requireAuth()) return '';
+    return view();
+  };
+}
 
-//restricting a route to only allow guest users
+// This view is for guest users only
 export function guestOnly(view) {
-    return() => {
-        if (!redirectIfLoggedIn()) return "";
-            return view();
-    };
+  return () => {
+    if (!redirectIfLoggedIn()) return '';
+    return view();
+  };
 }
